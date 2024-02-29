4c465100d52752cca768eb7b56ee9300fdcb2fbe
sujal-shukla <shuklasujal714@gmail.com>
2024-03-01 00:10:15 +0530

    Date Picker

--- src/app/(web)/rooms/[slug]/page.tsx
+++ src/app/(web)/rooms/[slug]/page.tsx
@@ -7,11 +7,16 @@ import { MdOutlineCleaningServices } from "react-icons/md";
 import { LiaFireExtinguisherSolid } from "react-icons/lia";
 import { AiOutlineMedicineBox } from "react-icons/ai";
 import { GiSmokeBomb } from "react-icons/gi";
+import BookRoomCta from "@/components/BookRoomCta/BookRoomCta";
+import { useState } from "react";
 
 const RoomDetails = (props: { params: { slug: string } }) => {
   const {
     params: { slug },
   } = props;
+
+  const [checkinDate, setCheckinDate] = useState<Date | null>(null)
+  const [checkoutDate, setCheckoutDate] = useState<Date | null>(null)
 
 
 
@@ -25,85 +30,109 @@ const RoomDetails = (props: { params: { slug: string } }) => {
 
   if (!room) return <LoadingSpinner />;
 
+  const calcMinCheckoutDate = () => {
+    if (checkinDate) {
+      const nextDay = new Date(checkinDate);
+      nextDay.setDate(nextDay.getDate() + 1);
+      return nextDay;
+    }
+    return null;
+  };
+
 
   return (
     <div>
-      <HotelPhotoGallery photos={room.images} />
+    <HotelPhotoGallery photos={room.images} />
 
-      <div className='container mx-auto mt-20'>
-        <div className='md:grid md:grid-cols-12 gap-10 px-3'>
-          <div className='md:col-span-8 md:w-full'>
-            <div>
-              <h2 className='font-bold text-left text-lg md:text-2xl'>
-                {room.name} ({room.dimension})
-              </h2>
-              <div className='flex my-11'>
+    <div className='container mx-auto mt-20'>
+      <div className='md:grid md:grid-cols-12 gap-10 px-3'>
+        <div className='md:col-span-8 md:w-full'>
+          <div>
+            <h2 className='font-bold text-left text-lg md:text-2xl'>
+              {room.name} ({room.dimension})
+            </h2>
+            <div className='flex my-11'>
+              {room.offeredAmenities.map(amenity => (
+                <div
+                  key={amenity._key}
+                  className='md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg-[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center'
+                >
+                  <i className={`fa-solid ${amenity.icon} md:text-2xl`}></i>
+                  <p className='text-xs md:text-base pt-3'>
+                    {amenity.amenity}
+                  </p>
+                </div>
+              ))}
+            </div>
+            <div className='mb-11'>
+              <h2 className='font-bold text-3xl mb-2'>Description</h2>
+              <p>{room.description}</p>
+            </div>
+            <div className='mb-11'>
+              <h2 className='font-bold text-3xl mb-2'>Offered Amenities</h2>
+              <div className='grid grid-cols-2'>
                 {room.offeredAmenities.map(amenity => (
                   <div
                     key={amenity._key}
-                    className='md:w-44 w-fit text-center px-2 md:px-0 h-20 md:h-40 mr-3 bg-[#eff0f2] dark:bg-gray-800 rounded-lg grid place-content-center'
+                    className='flex items-center md:my-0 my-1'
                   >
-                    <i className={`fa-solid ${amenity.icon} md:text-2xl`}></i>
-                    <p className='text-xs md:text-base pt-3'>
+                    <i className={`fa-solid ${amenity.icon}`}></i>
+                    <p className='text-xs md:text-base ml-2'>
                       {amenity.amenity}
                     </p>
                   </div>
                 ))}
               </div>
-              <div className='mb-11'>
-                <h2 className='font-bold text-3xl mb-2'>Description</h2>
-                <p>{room.description}</p>
-              </div>
-              <div className='mb-11'>
-                <h2 className='font-bold text-3xl mb-2'>Offered Amenities</h2>
-                <div className='grid grid-cols-2'>
-                  {room.offeredAmenities.map(amenity => (
-                    <div
-                      key={amenity._key}
-                      className='flex items-center md:my-0 my-1'
-                    >
-                      <i className={`fa-solid ${amenity.icon}`}></i>
-                      <p className='text-xs md:text-base ml-2'>
-                        {amenity.amenity}
-                      </p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className='mb-11'>
-                <h2 className='font-bold text-3xl mb-2'>Safety And Hygiene</h2>
-                <div className='grid grid-cols-2'>
-                  <div className='flex items-center my-1 md:my-0'>
-                    <MdOutlineCleaningServices />
-                    <p className='ml-2 md:text-base text-xs'>Daily Cleaning</p>
-                  </div>
-                  <div className='flex items-center my-1 md:my-0'>
-                    <LiaFireExtinguisherSolid />
-                    <p className='ml-2 md:text-base text-xs'>
-                      Fire Extinguishers
-                    </p>
-                  </div>
-                  <div className='flex items-center my-1 md:my-0'>
-                    <AiOutlineMedicineBox />
-                    <p className='ml-2 md:text-base text-xs'>
-                      Disinfections and Sterilizations
-                    </p>
-                  </div>
-                  <div className='flex items-center my-1 md:my-0'>
-                    <GiSmokeBomb />
-                    <p className='ml-2 md:text-base text-xs'>Smoke Detectors</p>
-                  </div>
-                </div>
-              </div>
-
-            <div className="md:col-span-4 rounded-xl shodow-lg dark:shadow dark:shadow-white sticky top-10 h-fit overflow-auto">
-              {/* Book Room Cta  */}
             </div>
+            <div className='mb-11'>
+              <h2 className='font-bold text-3xl mb-2'>Safety And Hygiene</h2>
+              <div className='grid grid-cols-2'>
+                <div className='flex items-center my-1 md:my-0'>
+                  <MdOutlineCleaningServices />
+                  <p className='ml-2 md:text-base text-xs'>Daily Cleaning</p>
+                </div>
+                <div className='flex items-center my-1 md:my-0'>
+                  <LiaFireExtinguisherSolid />
+                  <p className='ml-2 md:text-base text-xs'>
+                    Fire Extinguishers
+                  </p>
+                </div>
+                <div className='flex items-center my-1 md:my-0'>
+                  <AiOutlineMedicineBox />
+                  <p className='ml-2 md:text-base text-xs'>
+                    Disinfections and Sterilizations
+                  </p>
+                </div>
+                <div className='flex items-center my-1 md:my-0'>
+                  <GiSmokeBomb />
+                  <p className='ml-2 md:text-base text-xs'>Smoke Detectors</p>
+                </div>
+              </div>
+            </div>
+
+            {/* <div className='shadow dark:shadow-white rounded-lg p-6'>
+              <div className='items-center mb-4'>
+                <p className='md:text-lg font-semibold'>Customer Reviews</p>
+              </div>
+              <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                {/* <RoomReview roomId={room._id} /> */}
+              {/* </div> */}
+            {/* </div> */} 
           </div>
+        </div>
+
+        <div className='md:col-span-4 rounded-xl shadow-lg dark:shadow dark:shadow-white sticky top-10 h-fit overflow-auto'>
+          <BookRoomCta discount={room.discount} price={room.price} specialNote={room.specialNote}
+          checkinDate={checkinDate}
+          setCheckinDate={setCheckinDate}
+          checkoutDate={checkoutDate}
+          setCheckoutDate={setCheckoutDate}
+          calcMinCheckoutDate={calcMinCheckoutDate}
+          />
         </div>
       </div>
     </div>
-    </div>
+  </div>
   )
 }
 
