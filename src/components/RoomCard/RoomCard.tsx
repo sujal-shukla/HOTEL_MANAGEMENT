b3fbde7f5adca34feee76088176b431aeddf3524
sujal-shukla <shuklasujal714@gmail.com>
2024-02-29 22:50:59 +0530

    UPTO OFFERED AMENITIES

--- src/components/RoomCard/RoomCard.tsx
+++ src/components/RoomCard/RoomCard.tsx
@@ -33,7 +33,7 @@ const RoomCard: FC<Props> = props => {
 
         <p className='pt-2 text-xs'>{type} Room</p>
 
-        <p className='pt-3 pb-6'>{description.slice(1, 100)}...</p>
+        <p className='pt-3 pb-6'>{description.slice(0, 100)}...</p>
 
         <Link
           href={`/rooms/${slug.current}`}
