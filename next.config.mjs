127a9a17add244cfac953caf68d6a20b82e3fef6
sujal-shukla <shuklasujal714@gmail.com>
2024-02-29 19:13:30 +0530

    shayad ho jaaye

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images : {
+        domains : ["lh3.googleusercontent.com" , "images.unsplash.com" , "plus.unsplash.com" , "unsplash.com"]
+    }
+};
+
+export default nextConfig;
---
+++ src/libs/apis.ts
@@ -0,0 +1,33 @@
+import { Room } from '@/models/room';
+import sanityClient from './sanity';
+import * as queries from './sanityQueries';
+
+
+export async function getFeaturedRoom() {
+  const result = await sanityClient.fetch<Room>(
+    queries.getFeaturedRoomQuery,
+    {},
+    { cache: 'no-cache' }
+  );
+
+  return result;
+}
+
+export async function getRooms() {
+  const result = await sanityClient.fetch<Room[]>(
+    queries.getRoomsQuery,
+    {},
+    { cache: 'no-cache' }
+  );
+  return result;
+}
+
+export async function getRoom(slug: string) {
+  const result = await sanityClient.fetch<Room>(
+    queries.getRoom,
+    { slug },
+    { cache: 'no-cache' }
+  );
+
+  return result;
+}
